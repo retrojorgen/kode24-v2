c59821b8b0832e932c11d3549161daf5cb0ad8c0
Jørgen Jacobsen <jorgeja@gmail.com>
2020-12-02 21:05:53 +0100

    catch-all

---
+++ pages/[...all].js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1 className="title">
+        Heisannnnn
+      </h1>
+    </main>
+  </div>
+)
+
+export default Home
